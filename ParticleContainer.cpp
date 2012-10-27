e0e1d44f1cbf726e7985ae9eaaef1849b0c117d1
Florian-Dietz <florian.dietz2@gmx.de>
2012-10-27 11:21:19 +0200

    particle container branch initial
    not working due to some loading problems, I guess

---
+++ ParticleContainer.cpp
@@ -0,0 +1,39 @@
+/*
+ * ParticleContainer.h
+ *
+ *  Created on: 26.10.2012
+ *      Authors: Florian Dietz, Leonhard Spiegelberg
+ */
+
+#include "ParticleContainer.h"
+
+ParticleContainer() {
+	particles = new util::vector<Particle>();
+};
+
+ParticleContainer(util::vector<Particle> particles) {
+	this.particles = particles;
+};
+
+void AddParticle(Particle particle) {
+	particles.push_back(particle);
+};
+
+void Iterate(void(*func)(Particle)) {
+	for (util::vector<Particle>::iterator it=particles.begin() ; it < particles.end(); it++)
+	{
+		Particle p = *it;
+		(*func)(p);
+	}
+};
+
+void IteratePairwise(void(*func)(Particle, Particle)) {
+	for (util::vector<Particle>::iterator it1=particles.begin() ; it1 < particles.end(); it1++)
+		for (util::vector<Particle>::iterator it2=particles.begin() ; it2 < particles.end(); it2++)
+			if (it1 != it2)
+			{
+				Particle p1 = *it1;
+				Particle p2 = *it2;
+				(*func)(p1, p2);
+			}
+};
---
+++ ParticleContainer.h
@@ -0,0 +1,34 @@
+/*
+ * ParticleContainer.h
+ *
+ *  Created on: 26.10.2012
+ *      Authors: Florian Dietz, Leonhard Spiegelberg
+ */
+
+
+#ifndef PARTICLE_CONTAINER_H_
+#define PARTICLE_CONTAINER_H_
+
+#include "utils/Vector.h"
+#include "Particle.h"
+
+
+class ParticleContainer {
+
+private:
+	util::vector<Particle> particles;
+
+public:
+	ParticleContainer();
+
+	ParticleContainer(util::vector<Particle> particles);
+
+	void AddParticle(Particle particle);
+
+	void Iterate(void(*func)(Particle));
+	
+	void IteratePairwise(void(*func)(Particle, Particle));
+
+};
+
+#endif /* PARTICLE_CONTAINER_H_ */
