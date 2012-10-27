a5f9717340bce0083cdd6d60d0edc31b6a1af5b7
Florian-Dietz <florian.dietz2@gmx.de>
2012-10-27 21:09:10 +0200

    added doxygen commentary for two classes
    for Particle and ParticleContainer

--- src/ParticleContainer.cpp
+++ src/ParticleContainer.cpp
@@ -1,9 +1,17 @@
-/*
- * ParticleContainer.h
- *
- *  Created on: 26.10.2012
- *      Authors: Florian Dietz, Leonhard Spiegelberg
- */
+//------------------------------------------------------------------------------------------------
+// (C) 2012 by F.Dietz & L.Spiegelberg
+// include License here...
+
+//At the moment you must not distribute or use this code at all,
+//cause license details are not clear yet
+//If you want to receive updates feel free to subscribe
+//------------------------------------------------------------------------------------------------
+// File ParticleContainer.cpp
+// contains class ParticleContainer
+//------------------------------------------------------------------------------------------------
+/// @author F.Dietz
+/// @author L.Spiegelberg
+//------------------------------------------------------------------------------------------------
 
 #include "ParticleContainer.h"
 
@@ -20,6 +28,7 @@ void ParticleContainer::AddParticle(Particle particle) {
 }
 
 void ParticleContainer::Iterate(void(*func)(Particle)) {
+	/// iterate over all Particles and call the function on it
 	for (std::vector<Particle>::iterator it=particles.begin() ; it < particles.end(); it++)
 	{
 		Particle& p = *it;
@@ -28,10 +37,14 @@ void ParticleContainer::Iterate(void(*func)(Particle)) {
 }
 
 void ParticleContainer::IteratePairwise(void(*func)(Particle, Particle)) {
+	/// iterate over all Particles
 	for (std::vector<Particle>::iterator it1=particles.begin() ; it1 < particles.end(); it1++)
+		/// iterate over all Particles again, within the first loop
 		for (std::vector<Particle>::iterator it2=particles.begin() ; it2 < particles.end(); it2++)
+			/// make sure that a Particle is not paired with itself
 			if (it1 != it2)
 			{
+				/// call the function on the pair of Particles
 				Particle& p1 = *it1;
 				Particle& p2 = *it2;
 				(*func)(p1, p2);
--- src/ParticleContainer.h
+++ src/ParticleContainer.h
@@ -1,9 +1,17 @@
-/*
- * ParticleContainer.h
- *
- *  Created on: 26.10.2012
- *      Authors: Florian Dietz, Leonhard Spiegelberg
- */
+//------------------------------------------------------------------------------------------------
+// (C) 2012 by F.Dietz & L.Spiegelberg
+// include License here...
+
+//At the moment you must not distribute or use this code at all,
+//cause license details are not clear yet
+//If you want to receive updates feel free to subscribe
+//------------------------------------------------------------------------------------------------
+// File ParticleContainer.h
+// contains class ParticleContainer
+//------------------------------------------------------------------------------------------------
+/// @author F.Dietz
+/// @author L.Spiegelberg
+//------------------------------------------------------------------------------------------------
 
 
 #ifndef PARTICLE_CONTAINER_H_
@@ -12,21 +20,27 @@
 #include <vector>
 #include "Particle.h"
 
-
+/// a class that is used to store Particles and iterate over them
 class ParticleContainer {
 
 private:
+	/// the vector<Particles> that is used by the ParticleContainer to store the Particles
 	std::vector<Particle> particles;
 
 public:
+	/// a constructor that creates the value particles from scratch
 	ParticleContainer();
 
+	/// a constructor that takes one argument, which is the vector<Particles>
 	ParticleContainer(std::vector<Particle> particles);
 
+	/// a function to add a Particle to the ParticleContainer
 	void AddParticle(Particle particle);
 
+	/// a function that takes a void(*func)(Particle) and uses it to iterate over all Particles
 	void Iterate(void(*func)(Particle));
 	
+	/// a function that takes a void(*func)(Particle, Particle) and uses them to iterate over all pairs of Particles
 	void IteratePairwise(void(*func)(Particle, Particle));
 
 };
